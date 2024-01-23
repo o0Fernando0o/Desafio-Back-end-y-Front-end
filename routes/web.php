af6eba9e8defed4cd46d8e275be6e6b4dee36aa0
o0Fernando0o <2519360028fhernandezc@gmail.com>
2024-01-23 07:48:40 -0600

    Se realizaron nuevas modificaciones en las rutas

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::group(['prefix' => 'guests'], function () {
     Route::post('/', [GuestController::class, 'store'])->name('guests.store');
     Route::get('/{id}', [GuestController::class, 'show'])->name('guests.show');
     Route::get('/{id}/edit', [GuestController::class, 'edit'])->name('guests.edit');
-    Route::put('/{guests}', [GuestController::class, 'update'])->name('guests.update');
+    Route::put('/{guest}', [GuestController::class, 'update'])->name('guests.update');
     Route::delete('/{id}', [GuestController::class, 'destroy'])->name('guests.destroy');
 
     // Rutas personalizadas
@@ -26,11 +26,6 @@ Route::group(['prefix' => 'guests'], function () {
     Route::get('/guest/filter', [GuestController::class, 'filterByBirthdate'])->name('guest.filterByBirthdate');
 });
 
-
-
-// Route::resource('guests', GuestController::class);
-// Route::get('/guest/search', [GuestController::class, 'searchByName'])->name('guest.searchByName');
-// Route::get('/guest/filter', [GuestController::class, 'filterByBirthdate'])->name('guest.filterByBirthdate');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
